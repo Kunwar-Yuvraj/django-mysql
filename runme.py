5528d183ba1d06875eccd9950c8bc600f8c6fd7a
kunwar-yuvraj <cppprograminglearner@gmail.com>
2021-02-22 20:04:38 +0530

    For Windows

---
+++ runme.py
@@ -0,0 +1,31 @@
+import os, time
+import mysql.connector
+import webbrowser
+
+username = input("Enter mysql user name: ")
+password = input("Enter mysql password: ")
+
+try:
+	conn = mysql.connector.connect(host = 'localhost', user = username, passwd= password)
+	print("Connected")
+	print()
+	print("1. Wait, program is opening")
+	print("2. You have to enter password again if needed!!")
+	
+	os.system(f'cmd /c "mysql -u {username} -p < database/students.sql"')
+
+	try:
+		os.rename('database/students.sql','database/students.sql.old')
+	except:
+		pass
+                
+	
+	print("3. Go to 'http://127.0.0.1:8000'")
+	
+	os.system('cmd /k "python manage.py runserver"')
+
+
+
+
+except Exception as e:
+	print(e)
